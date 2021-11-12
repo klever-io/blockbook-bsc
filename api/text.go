1fbfe19026852c1bba83d8559d327e9549cdd825
Pedro Ivo <pi3virgula14@outlook.com>
2021-11-12 18:13:04 -0300

    Fix panic problem

--- api/text.go
+++ api/text.go
@@ -1,33 +1,11 @@
 package api
 
-import (
-	"fmt"
-	"net/url"
-	"strings"
-
-	"github.com/gobuffalo/packr"
-)
-
 // Text contains static overridable texts used in explorer
 var Text struct {
 	BlockbookAbout, TOSLink string
 }
 
 func init() {
-	box := packr.NewBox("../build/text")
-	if about, err := box.MustString("about"); err == nil {
-		Text.BlockbookAbout = strings.TrimSpace(about)
-	} else {
-		panic(err)
-	}
-	if tosLink, err := box.MustString("tos_link"); err == nil {
-		tosLink = strings.TrimSpace(tosLink)
-		if _, err := url.ParseRequestURI(tosLink); err == nil {
-			Text.TOSLink = tosLink
-		} else {
-			panic(fmt.Sprint("tos_link is not valid URL:", err.Error()))
-		}
-	} else {
-		panic(err)
-	}
+	Text.BlockbookAbout = "Blockbook created by Trezor and deployed by klever :)"
+	Text.TOSLink = "https://wallet.trezor.io/tos.pdf"
 }
