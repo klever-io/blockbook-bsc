67bd19b6141eb3da7a08d646c3440fe97934c685
Pedro Ivo <pi3virgula14@outlook.com>
2021-11-12 18:28:12 -0300

    test if "coisa legal"

--- api/text.go
+++ api/text.go
@@ -1,11 +1,12 @@
 package api
 
+import "fmt"
+
 // Text contains static overridable texts used in explorer
 var Text struct {
 	BlockbookAbout, TOSLink string
 }
 
 func init() {
-	Text.BlockbookAbout = "Blockbook created by Trezor and deployed by klever :)"
-	Text.TOSLink = "https://wallet.trezor.io/tos.pdf"
+	fmt.Println("COISA LEGAL")
 }
