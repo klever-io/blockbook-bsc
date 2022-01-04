27e0892de790a587eef521a4d85b0018de664e96
Vinicius Niche Correa <viniciusnichecorrea@gmail.com>
2022-01-04 17:47:12 -0300

    fix bsc erc20 out of bounds (#1)
    * fix out of bounds
    
    * fix operator
    
    * fix operator
    
    * update build files
    
    * update build files 2
    
    * remove duplicated transaction receipt
    
    * downgrade rocks
    
    * Fix panic problem
    
    * test if "coisa legal"
    
    * fix "coisa legal"
    
    * update dockerfile
    
    Co-authored-by: Pedro Ivo <pi3virgula14@outlook.com>

--- api/text.go
+++ api/text.go
@@ -1,33 +1,11 @@
 package api
 
-import (
-	"fmt"
-	"net/url"
-	"strings"
-
-	"github.com/gobuffalo/packr"
-)
-
 // Text contains static overridable texts used in explorer
 var Text struct {
 	BlockbookAbout, TOSLink string
 }
 
 func init() {
-	box := packr.NewBox("../build/text")
-	if about, err := box.MustString("about"); err == nil {
-		Text.BlockbookAbout = strings.TrimSpace(about)
-	} else {
-		panic(err)
-	}
-	if tosLink, err := box.MustString("tos_link"); err == nil {
-		tosLink = strings.TrimSpace(tosLink)
-		if _, err := url.ParseRequestURI(tosLink); err == nil {
-			Text.TOSLink = tosLink
-		} else {
-			panic(fmt.Sprint("tos_link is not valid URL:", err.Error()))
-		}
-	} else {
-		panic(err)
-	}
+	Text.BlockbookAbout = "Blockbook - blockchain indexer for Trezor wallet https://trezor.io/. Do not use for any other purpose."
+	Text.TOSLink = "https://wallet.trezor.io/tos.pdf"
 }
